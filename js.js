17e6e8441c37623ba43e4073c5395117321e270a
GUCHIMINA <44945819+m-guchi@users.noreply.github.com>
2020-11-25 17:24:08 +0900

    Add files via upload

--- js.js
+++ js.js
@@ -1,6 +1,8 @@
+var datanum = 0;
+
 function click(event){
     if (event.key == "ArrowDown"){
-        if(id >= 3) return 0
+        if (id >= datanum-1) return 0
         document.getElementById("item-" + id).style.visibility = "hidden";
         id += 1;
     } else if (event.key == "ArrowUp") {
@@ -12,20 +14,42 @@ function click(event){
     document.getElementById("item-" + id).style.visibility = "visible";
 }
 
-function getCsvData(dataPath) {
-    const request = new XMLHttpRequest();
-    request.addEventListener('load', (event) => {
-        const response = event.target.responseText;
-        outputElement.innerHTML = response;
-    });
-    request.open('GET', dataPath, true);
-    request.send();
+const getCSV = new Promise((resolve,reject) => {
+    var req = new XMLHttpRequest();
+    req.open("get", "./data.csv", true);
+    req.overrideMimeType('text/plain; charset=Shift_JIS');
+    req.send(null); 
+    req.onload = function () {
+        convertCSVtoArray(req.responseText);
+        resolve(true)
+    }
+})
+
+function convertCSVtoArray(str) { 
+    var result = [];
+    var tmp = str.split("\n");
+    for (var i = 0; i < tmp.length; ++i) {
+        result[i] = tmp[i].split(',');
+    }
+    
+    datanum = result.length * 2
+    
+    var textbox = ''
+    for(var i=0;i<result.length;i++) {
+        console.log(i)
+        textbox += '<div class="items">'
+            + '<div class="item" id="item-'+(i*2)+'"><span class="english" id="test">' + result[i][0] + '</span></div>'
+            + '<div class="item" id="item-'+(i*2+1)+'"><span class="japanese">' + result[i][1] + '</span></div>'
+            + '</div>'
+    }
+
+    document.getElementById("textbox").innerHTML = textbox
 }
 
 window.onload = function(){
-    const outputElement = document.getElementById('output_csv');
-    getCsvData('./data.csv');
-    document.getElementById("item-0").style.visibility = "visible";
+    getCSV.then((res) => {
+        if(res) document.getElementById("item-0").style.visibility = "visible";
+    })
 }
 
 let id = 0;
